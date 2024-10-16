0204752a44daafd404066740f0535ec4150fa695
LiaDAO <97869178+Han-Na-05-22@users.noreply.github.com>
2024-10-16 13:55:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,7 @@
+const solution = (my_string) => {
+    return [...my_string]
+        .map((string) =>
+            /[a-z]/g.test(string) ? string.toUpperCase() : string.toLowerCase(),
+        )
+        .join('');
+};
